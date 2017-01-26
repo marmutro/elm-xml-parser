7ca8514f0b851b358fd563d0088458330c0cd8a5
marmutro <marmutro@gmail.com>
2017-01-26 09:14:08 +0100

    Applied elm-format

--- Xml/Decode.elm
+++ Xml/Decode.elm
@@ -1,6 +1,5 @@
 module Xml.Decode exposing (toJson)
 
-
 {-| The Decoder converts an XML AST to an Json value
 
 @docs toJson
@@ -9,109 +8,128 @@ module Xml.Decode exposing (toJson)
 import Tuple exposing (first, second)
 import Json.Encode exposing (string, object, list, array)
 import Xml.Parser exposing (XmlAst(..))
+
+
 --import List.Extra as List
+
 import Array
 
 
 takeWhile : (a -> Bool) -> List a -> List a
 takeWhile predicate list =
-  case list of
-    []      -> []
-    x::xs   -> if (predicate x) then x :: takeWhile predicate xs
-               else []
+    case list of
+        [] ->
+            []
+
+        x :: xs ->
+            if (predicate x) then
+                x :: takeWhile predicate xs
+            else
+                []
 
 
 dropWhile : (a -> Bool) -> List a -> List a
 dropWhile predicate list =
-  case list of
-    []      -> []
-    x::xs   -> if (predicate x) then dropWhile predicate xs
-               else list
+    case list of
+        [] ->
+            []
+
+        x :: xs ->
+            if (predicate x) then
+                dropWhile predicate xs
+            else
+                list
 
 
-span : (a -> Bool) -> List a -> (List a, List a)
-span p xs = (takeWhile p xs, dropWhile p xs)
+span : (a -> Bool) -> List a -> ( List a, List a )
+span p xs =
+    ( takeWhile p xs, dropWhile p xs )
 
 
 groupBy : (a -> a -> Bool) -> List a -> List (List a)
 groupBy eq xs1 =
-  case xs1 of
-    [] -> []
-    (x::xs) -> let (ys,zs) = span (eq x) xs
-               in (x::ys)::groupBy eq zs
+    case xs1 of
+        [] ->
+            []
+
+        x :: xs ->
+            let
+                ( ys, zs ) =
+                    span (eq x) xs
+            in
+                (x :: ys) :: groupBy eq zs
 
 
 getElementsOrArray : List XmlAst -> List ( String, Json.Encode.Value )
 getElementsOrArray elems =
-  let
-    -- partition the list by determining if a given key exists more than one time -> array
-    ( array1, elems1 ) =
-      elems
-        |> List.map toJson1
-        |> groupBy (\a b -> first a == first b)
-        |> List.partition (\ls -> List.length ls > 1)
+    let
+        -- partition the list by determining if a given key exists more than one time -> array
+        ( array1, elems1 ) =
+            elems
+                |> List.map toJson1
+                |> groupBy (\a b -> first a == first b)
+                |> List.partition (\ls -> List.length ls > 1)
 
-    -- flatten the "normal" elements
-    elems2 =
-      elems1
-        |> List.concatMap identity
+        -- flatten the "normal" elements
+        elems2 =
+            elems1
+                |> List.concatMap identity
 
-    -- create the array elements
-    array2 =
-      array1
-        |> List.map
-            (\ls ->
-              let
-                name =
-                  ls
-                    |>
-                      List.head
-                    |>
-                      Maybe.map first
-                    -- cannot happen, there is always at least one element
-                    -- in the list by the nature of how groupBy works
-                    |>
-                      Maybe.withDefault ""
-              in
-                ( name
-                , ls
-                    |> List.map second
-                    |> Array.fromList
-                    |> array
-                )
-            )
-  in
-    elems2 ++ array2
+        -- create the array elements
+        array2 =
+            array1
+                |> List.map
+                    (\ls ->
+                        let
+                            name =
+                                ls
+                                    |> List.head
+                                    |> Maybe.map first
+                                    -- cannot happen, there is always at least one element
+                                    -- in the list by the nature of how groupBy works
+                                    |>
+                                        Maybe.withDefault ""
+                        in
+                            ( name
+                            , ls
+                                |> List.map second
+                                |> Array.fromList
+                                |> array
+                            )
+                    )
+    in
+        elems2 ++ array2
 
 
 getAttributes : List ( String, String ) -> List ( String, Json.Encode.Value )
 getAttributes attrs =
-  attrs
-    |> List.map (\( n, v ) -> ( "__" ++ n, string v ))
+    attrs
+        |> List.map (\( n, v ) -> ( "__" ++ n, string v ))
 
 
 toJson1 : XmlAst -> ( String, Json.Encode.Value )
 toJson1 xmlAst =
-  case xmlAst of
-    Body txt ->
-      ( "__text", string txt )
+    case xmlAst of
+        Body txt ->
+            ( "__text", string txt )
 
-    Comment comment ->
-      ( "__comment", string comment )
+        Comment comment ->
+            ( "__comment", string comment )
 
-    CDATA cdata ->
-      ( "__cdata", string cdata )
+        CDATA cdata ->
+            ( "__cdata", string cdata )
 
-    Element name [] [ Body txt ] ->
-      ( name, string txt )
+        Element name [] [ Body txt ] ->
+            ( name, string txt )
 
-    Element name attrs elems ->
-      ( name, object (getAttributes attrs ++ getElementsOrArray elems) )
+        Element name attrs elems ->
+            ( name, object (getAttributes attrs ++ getElementsOrArray elems) )
 
 
-{-| Converts an XML AST to a Json value -}
+{-| Converts an XML AST to a Json value
+-}
 toJson : List XmlAst -> Json.Encode.Value
 toJson xmlAst =
-  xmlAst
-    |> List.map toJson1
-    |> object
+    xmlAst
+        |> List.map toJson1
+        |> object
--- Xml/Parser.elm
+++ Xml/Parser.elm
@@ -1,6 +1,5 @@
 module Xml.Parser exposing (parseXml, XmlAst(..))
 
-
 {-| A parser which converts XML into an XmlAst,
 which can be further transformed.
 
@@ -13,159 +12,161 @@ import String
 
 
 type alias Name =
-  String
+    String
 
 
 type alias Key =
-  String
+    String
 
 
 type alias Value =
-  String
+    String
 
 
 type alias Attribute =
-  ( Key, Value )
+    ( Key, Value )
 
 
-{-| The XML AST representation -}
+{-| The XML AST representation
+-}
 type XmlAst
-  = Element Name (List Attribute) (List XmlAst)
-  | Body String
-  | Comment String
-  | CDATA String
+    = Element Name (List Attribute) (List XmlAst)
+    | Body String
+    | Comment String
+    | CDATA String
 
 
 spaces : Parser s (List Char)
 spaces =
-  many (space <|> newline <|> tab)
+    many (space <|> newline <|> tab)
 
 
 letter : Parser s Char
 letter =
-  upper <|> lower
+    upper <|> lower
 
 
 betweenBoth : Char -> Parser s String
 betweenBoth ch =
-  String.fromList
-    <$> between
-          (char ch)
-          (char ch)
-          (many1 ((noneOf [ ch ])) <|> succeed [])
+    String.fromList
+        <$> between
+                (char ch)
+                (char ch)
+                (many1 ((noneOf [ ch ])) <|> succeed [])
 
 
 betweenSingleQuotes : Parser s String
 betweenSingleQuotes =
-  betweenBoth '\''
+    betweenBoth '\''
 
 
 betweenDoubleQuotes : Parser s String
 betweenDoubleQuotes =
-  betweenBoth '"'
+    betweenBoth '"'
 
 
 quotedString : Parser s String
 quotedString =
-  betweenSingleQuotes <|> betweenDoubleQuotes
+    betweenSingleQuotes <|> betweenDoubleQuotes
 
 
 attributeName : Parser s String
 attributeName =
-  String.fromList
-    <$> many1 (letter <|> digit <|> char '-' <|> char ':')
-    <?> "Invalid Attribute name"
+    String.fromList
+        <$> many1 (letter <|> digit <|> char '-' <|> char ':')
+        <?> "Invalid Attribute name"
 
 
 tagName : Parser s String
 tagName =
-  String.fromList
-    <$> many (choice [ letter, digit, char '_', char ':' ])
-    <?> "Invalid Tag name"
+    String.fromList
+        <$> many (choice [ letter, digit, char '_', char ':' ])
+        <?> "Invalid Tag name"
 
 
 keyValue : Parser s ( String, String )
 keyValue =
-  (\key value -> ( key, value ))
-    <$> (attributeName <* spaces <* char '=' <* spaces)
-    <*> (quotedString <* spaces)
+    (\key value -> ( key, value ))
+        <$> (attributeName <* spaces <* char '=' <* spaces)
+        <*> (quotedString <* spaces)
 
 
 openTag : Parser s ( String, List ( String, String ) )
 openTag =
-  (\name attribs -> ( name, attribs ))
-    <$> (char '<' *> tagName)
-    <*> (spaces *> many keyValue <* char '>')
+    (\name attribs -> ( name, attribs ))
+        <$> (char '<' *> tagName)
+        <*> (spaces *> many keyValue <* char '>')
 
 
 closeTag : String -> Parser s ()
 closeTag str =
-  ()
-    <$ (string "</" *> spaces *> string str *> spaces *> char '>')
-    <?> ("Expected closing Tag for " ++ toString str)
+    ()
+        <$ (string "</" *> spaces *> string str *> spaces *> char '>')
+        <?> ("Expected closing Tag for " ++ toString str)
 
 
 withExplicitCloseTag : Parser s XmlAst
 withExplicitCloseTag =
-  (\( name, attribs, xml ) -> Element name attribs xml)
-    <$> ((openTag <* spaces) >>= \( name, attribs ) -> (\xml -> ( name, attribs, xml )) <$> (many (innerXml <* spaces) <* closeTag name))
+    (\( name, attribs, xml ) -> Element name attribs xml)
+        <$> ((openTag <* spaces) >>= \( name, attribs ) -> (\xml -> ( name, attribs, xml )) <$> (many (innerXml <* spaces) <* closeTag name))
 
 
 comment : Parser s XmlAst
 comment =
-  (String.fromList >> String.trim >> Comment)
-    <$> (string "<!--" *> manyTill anyChar (string "-->"))
+    (String.fromList >> String.trim >> Comment)
+        <$> (string "<!--" *> manyTill anyChar (string "-->"))
 
 
 cdata : Parser s XmlAst
 cdata =
-  (String.fromList >> String.trim >> CDATA)
-    <$> (string "<![CDATA[" *> manyTill anyChar (string "]]>"))
+    (String.fromList >> String.trim >> CDATA)
+        <$> (string "<![CDATA[" *> manyTill anyChar (string "]]>"))
 
 
 withoutExplicitCloseTag : Parser s XmlAst
 withoutExplicitCloseTag =
-  (\name attribs -> Element name attribs [])
-    <$> ((char '<' *> tagName <* spaces))
-    <*> (many keyValue <* string "/>")
+    (\name attribs -> Element name attribs [])
+        <$> ((char '<' *> tagName <* spaces))
+        <*> (many keyValue <* string "/>")
 
 
 parseBody : Parser s XmlAst
 parseBody =
-  (Body << String.trim << String.fromList) <$> (many1 (noneOf [ '<', '>' ]))
+    (Body << String.trim << String.fromList) <$> (many1 (noneOf [ '<', '>' ]))
 
 
 xmlDeclaration : Parser s ()
 xmlDeclaration =
-  () <$ (string "<?xml" *> Combine.while ((/=) '?') <* string "?>")
+    () <$ (string "<?xml" *> Combine.while ((/=) '?') <* string "?>")
 
 
 xmlParser : Parser s XmlAst
 xmlParser =
-  lazy (\() -> withExplicitCloseTag) <|> lazy (\() -> withoutExplicitCloseTag)
+    lazy (\() -> withExplicitCloseTag) <|> lazy (\() -> withoutExplicitCloseTag)
 
 
 rootElements : Parser s (List XmlAst)
 rootElements =
-  many1 (choice [ xmlParser, comment <* spaces ])
+    many1 (choice [ xmlParser, comment <* spaces ])
 
 
 innerXml : Parser s XmlAst
 innerXml =
-  comment <|> cdata <|> xmlParser <|> parseBody
+    comment <|> cdata <|> xmlParser <|> parseBody
 
 
 parser : Parser s (List XmlAst)
 parser =
-  spaces *> maybe xmlDeclaration *> spaces *> rootElements <* spaces <* end
+    spaces *> maybe xmlDeclaration *> spaces *> rootElements <* spaces <* end
 
 
-{-| Trys to parse the input string as an AST -}
+{-| Trys to parse the input string as an AST
+-}
 parseXml : String -> Result (List String) (List XmlAst)
 parseXml str =
-  case parse parser str of
-    Ok (_, _, xml ) ->
-      Ok xml
+    case parse parser str of
+        Ok ( _, _, xml ) ->
+            Ok xml
 
-    Err (_, _, failure ) ->
-      Err failure
+        Err ( _, _, failure ) ->
+            Err failure
